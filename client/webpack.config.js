f1f5987fd27ca63606d1e4d0be7e100415368473
Mohammad Irfan <v-moirf@microsoft.com>
2023-02-01 00:17:34 +0000

    adding server js for deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,8 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
+      template: "./public/index.html"
     }),
   ],
   devServer: {
